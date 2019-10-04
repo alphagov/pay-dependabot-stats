3efa34309a386d04534725e7946e745d95b0df31
Rory Malcolm <rory.malcolm@digital.cabinet-office.gov.uk>
2019-10-04 11:25:56 +0100

    Update title and day of the week convertor

--- src/Containers/DateContainer.tsx
+++ src/Containers/DateContainer.tsx
@@ -28,19 +28,19 @@ class DateContainer extends Component<DateContainerProps, DateContainerState> {
 
   parseDayOfWeek(index: number): string {
     switch (index) {
-      case 0:
-        return "Monday";
       case 1:
-        return "Tuesday";
+        return "Monday";
       case 2:
-        return "Wednesday";
+        return "Tuesday";
       case 3:
-        return "Thursday";
+        return "Wednesday";
       case 4:
-        return "Friday";
+        return "Thursday";
       case 5:
-        return "Saturday";
+        return "Friday";
       case 6:
+        return "Saturday";
+      case 7:
         return "Sunday";
       default:
         return "ParseError";
