65dc16bdbbe6d84e33d47ac77e4aa032090dcd55
Rory Malcolm <rory.malcolm@digital.cabinet-office.gov.uk>
2019-10-07 08:56:14 +0100

    Update label type syntax

--- src/types/Label.ts
+++ src/types/Label.ts
@@ -1,8 +1,8 @@
 export interface Label { 
-        "id": number,
-        "node_id": string,
-        "url": string,
-        "name": string,
-        "color": string,
-        "default": boolean
+        id: number,
+        node_id: string,
+        url: string,
+        name: string,
+        color: string,
+        default: boolean
 }
